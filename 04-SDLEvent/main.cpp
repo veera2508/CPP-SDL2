2c96385a00882d0df0ff1ece5666be434def72b1
Veeraraghavan <virunarasimhan25@gmail.com>
2021-05-10 20:28:15 +0530

    changes

---
+++ 04-SDLEvent/main.cpp
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <SDL2/SDL.h>
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+bool init();
+bool loadMedia();
+void close();
+
+SDL_Window* gWindow = NULL;
+SDL_Surface* gScreenSurface = NULL;
+SDL_Surface* gHelloWorld = NULL;
+
+bool init() {
+    bool success = true;
+    if ( SDL_Init( SDL_INIT_VIDEO ) < 0) {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        success = false;
+    }
+    else {
+        gWindow = SDL_CreateWindow( "SDL_Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        if (gWindow == NULL){
+            printf("Window could not be created! SDL_Error: %s\n");
+            success = false;
+        }
+        else {
+            gScreenSurface = SDL_GetWindowSurface( gWindow );
+        }
+    }
+    return success;
+}
+
+bool loadMedia() {
+    bool success = true;
+    gHelloWorld = SDL_LoadBMP("sample.bmp");
+    if (gHelloWorld == NULL) {
+        printf("Unable to load image! SDL Error: %s\n", SDL_GetError());
+        success = false;
+    }
+    return success;
+}
+
+void close() {
+    SDL_FreeSurface( gHelloWorld );
+    gHelloWorld = NULL;
+
+    SDL_DestroyWindow( gWindow );
+    gWindow = NULL;
+
+    SDL_Quit();
+}
+
+int main( int argc, char *args[] ) {
+    if (!init()) {
+        printf("Failed to initialize!\n");
+    }
+    else {
+        if ( !loadMedia() ) {
+            printf("Failed to load Media!\n");
+        }
+        else {
+            bool quit = false;
+            SDL_Event e;
+            while (!quit) {
+                while (SDL_PollEvent( &e ) != 0) {
+                    if (e.type == SDL_QUIT)
+                        quit = true;
+                }
+                SDL_BlitSurface(gHelloWorld, NULL, gScreenSurface, NULL);
+                SDL_UpdateWindowSurface(gWindow);
+            }
+        }
+    }
+    close();
+    return 0;
+} 
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # CPP-SDL2
 Sample programs in CPP using the SDL 2 Library
+Done following the tutorial by Lazyfoo at https://www.lazyfoo.net/tutorials/SDL/index.php
