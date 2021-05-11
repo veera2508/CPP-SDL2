dded3959f9d694ebbc3f98a55ab857e96fdc050d
Veeraraghavan <virunarasimhan25@gmail.com>
2021-05-11 18:02:52 +0530

    added geometry

--- 08-Geometry/main.cpp
+++ 08-Geometry/main.cpp
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
 #include <string>
 
 const int SCREEN_WIDTH = 640;
@@ -36,56 +35,14 @@ bool init() {
                 printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
                 success = false;
             }
-            else {
-                //Initialize renderer color
-                SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
-
-                //Initialize loading for png
-                int imgFlags = IMG_INIT_PNG; //flag for png loading
-                //We pass the flags required to load and it returns flags successfully loaded
-                if ( !( IMG_Init( imgFlags ) & imgFlags ) ) {
-                    printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
-                    success = false;
-                }
-            }
-        }
-        
+        }  
     }
     return success;
 }
 
-//Load image and return texture
-SDL_Texture* loadTexture( std::string path ) {
-    //Surface to store the optimized image
-    SDL_Texture* newTexture = NULL;
-
-    //Surface to store the image
-    SDL_Surface* loadedSurface = IMG_Load(path.c_str());
-    if ( loadedSurface == NULL )
-        printf( "Unable to load image! SDL Error: %s\n", IMG_GetError() );
-    
-    else {
-        //Create texture from surface pixels
-
-        newTexture = SDL_CreateTextureFromSurface(gRenderer, loadedSurface);
-        if (newTexture == NULL) 
-            printf("Unable to create texture! SDL Error:%s\n", SDL_GetError());
-        
-        //get rid of old surface
-        SDL_FreeSurface(loadedSurface);
-    }
-    return newTexture;
-}
 
 bool loadMedia() {
     bool success = true;
-
-    gTexture = loadTexture( "texture.png" );
-	if( gTexture == NULL ){
-		printf( "Failed to load texture image!\n" );
-		success = false;
-	}
-
 	return success;
 }
 
@@ -101,7 +58,6 @@ void close() {
     SDL_DestroyWindow( gWindow );
     gWindow = NULL;
 
-    IMG_Quit();
     SDL_Quit();
 }
 
@@ -124,10 +80,26 @@ int main( int argc, char *args[] ) {
                     if (e.type == SDL_QUIT)
                         quit = true;
                 }
+                //Initialize renderer color
+                SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
                 //Clear Screen
                 SDL_RenderClear( gRenderer );
-                //Copy portion or whole of texture to the renderer
-                SDL_RenderCopy( gRenderer, gTexture, NULL, NULL );
+                //Create and Draw rectangle filled with red
+                SDL_Rect fillRect = {SCREEN_WIDTH/4, SCREEN_HEIGHT/4, SCREEN_WIDTH/2, SCREEN_HEIGHT/2};
+                SDL_SetRenderDrawColor( gRenderer, 0xFF, 0x00, 0x00, 0xFF );
+                SDL_RenderFillRect( gRenderer, &fillRect );
+                //Create and Draw hollow rectangle with black outline
+                SDL_Rect outlineRect = {SCREEN_WIDTH/6, SCREEN_HEIGHT/6, 2*SCREEN_WIDTH/3, 2*SCREEN_HEIGHT/3};
+                SDL_SetRenderDrawColor( gRenderer, 0x00, 0x00, 0x00, 0xFF );
+                SDL_RenderDrawRect( gRenderer, &outlineRect );
+                //Draw green horizontal line
+                SDL_SetRenderDrawColor( gRenderer, 0x00, 0xFF, 0x00, 0x00 );
+                SDL_RenderDrawLine( gRenderer, 0, SCREEN_HEIGHT/2, SCREEN_WIDTH, SCREEN_HEIGHT/2 );
+                //Draw vertical line of blue dots
+                SDL_SetRenderDrawColor( gRenderer, 0x00, 0x00, 0xFF, 0x00 );
+                for (int i = 0; i < SCREEN_HEIGHT; i+=5) {
+                    SDL_RenderDrawPoint( gRenderer, SCREEN_WIDTH/2, i );
+                }
                 //Update the renderer
                 SDL_RenderPresent( gRenderer );
             }
